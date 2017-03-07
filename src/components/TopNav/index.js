8c28ff0c625ebf5a6c6e028b9b03e1a1dbdf1601
Sergey <oseckijsergej@gmail.com>
2017-03-07 17:19:09 +0200

    add toggle sidebar

--- src/components/TopNav/index.js
+++ src/components/TopNav/index.js
@@ -1,11 +1,11 @@
 import React, {PropTypes, Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import * as actions from './TopNavActions';
+import * as actions from './topNavActions';
 
 @connect(
   (state, ownProps) => ({
-    state: state
+    isToggle: state.toggleSidebar
   }),
   dispatch => ({
     actions: bindActionCreators(actions, dispatch)
@@ -15,6 +15,24 @@ export default class TopNav extends Component {
     // prop: PropTypes.object.isRequired,
   };
 
+  componentDidUpdate() {
+    let { isToggle } = this.props;
+    this.toggleSidebar(isToggle);
+  }
+
+  toggleSidebar(isToggle) {
+    if (isToggle) {
+      document.getElementById('pageBody').classList.add('mini-navbar');
+    } else {
+      document.getElementById('pageBody').classList.remove('mini-navbar');
+    }
+  }
+
+  onBurgerClick = (e) => {
+    e.preventDefault();
+    this.props.actions.toggleSidebar();
+  };
+
   render() {
 
     return (
@@ -22,7 +40,11 @@ export default class TopNav extends Component {
 
         <nav className="navbar navbar-static-top white-bg" role="navigation" style={{marginBottom: 0}}>
           <div className="navbar-header">
-            <a className="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i className="fa fa-bars" /> </a>
+            <a
+              onClick={this.onBurgerClick}
+              className="navbar-minimalize minimalize-styl-2 btn btn-primary ">
+              <i className="fa fa-bars" />
+            </a>
           </div>
           <ul className="nav navbar-top-links navbar-right">
             <li>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 import { routerReducer } from 'react-router-redux';
 import monitorItems from '../components/Monitor/IndexMonitor/monitorReducer';
+import toggleSidebar from '../components/TopNav/topNavReducer';
 
 const rootReducer = combineReducers({
   form,
   routing: routerReducer,
   monitorItems,
+  toggleSidebar
 });
 
 export default rootReducer;
